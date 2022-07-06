4b6ac171d463a580d699cffa02112734f570d726
tit-22 <108799143+tit-22@users.noreply.github.com>
2022-07-06 17:27:25 +0700

    Проба

---
+++ probe.js
@@ -0,0 +1,7 @@
+<div id="main">
+ <div class="inner">
+   <header>
+     <h1>Последние новости мира компьютеров.</h1>
+     <p>Если у Вас будут какие либо вопросы, внизу страницы есть обратная связь, напишите нам.</p>
+<input id="spterm" type="text" name="spterm" placeholder="Что искать?">
+				<div id="spresult">&nbsp;</div>
